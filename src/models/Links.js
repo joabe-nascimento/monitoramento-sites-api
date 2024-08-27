63297b91a1880bdb7099cf854c6df59c7f2fa0e8
joabe-nascimento <joabefnascimento1@outlook.com>
2024-08-27 10:32:10 -0300

    mudanças

--- src/models/Links.js
+++ src/models/Links.js
@@ -1,5 +1,7 @@
+// Importa o Mongoose, que é uma biblioteca para modelar objetos MongoDB
 const mongoose = require('mongoose');
 
+// Define um esquema (Schema) para os documentos da coleção "administradores"
 const linksSchema = new mongoose.Schema({
     link: { type: String, required: true }, // Campo "usuario" do tipo String, obrigatório
     created_at: { type: Date, default: Date.now }, // Adiciona o campo created_at com a data atual
