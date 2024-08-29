6f17248e0376ca20b953d4d1720831be3de34665
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-08-29 14:12:06 -0300

    ajuste de api

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-require('dotenv').config(); // Para carregar variáveis de ambiente
-const axios = require('axios');
-const nodemailer = require('nodemailer');
-const express = require('express');
-const cors = require('cors');
-const https = require('https');
-const connectDataBase = require('./database/db.js');
-const Links = require('./models/Links.js');
+const axios = require("axios");
+const nodemailer = require("nodemailer");
+const express = require("express");
+const cors = require("cors");
+const https = require("https");
+
+const connectDataBase = require("./database/db.js");
+const Links = require("./models/Links.js");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 
 const siteStates = {};
-const retryAttempts = 3;
-const retryDelay = 2000;
-
-let lastMessageTime = 0;
-const minInterval = 10 * 60 * 1000;
-const maxInterval = 30 * 60 * 1000;
+const retryAttempts = 3; // Número de tentativas antes de considerar o site como offline
+const retryDelay = 2000; // Tempo de espera entre as tentativas em milissegundos
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "seu-email@gmail.com",
+    pass: "sua-senha-do-email",
   },
 });
 
@@ -35,48 +31,42 @@ const agent = new https.Agent({
   rejectUnauthorized: false,
 });
 
-const updownApiKey = process.env.UPDOWN_API_KEY;
-
 async function sendTelegramMessage(message) {
-  const now = Date.now();
+  const telegramBotToken = "7472348745:AAGMqF50_Q4TAWyQgeJySb0tG-njguiJmrI";
+  const telegramChatId = "-1002155037998";
 
-  if (now - lastMessageTime >= minInterval) {
-    if (lastMessageTime === 0 || now - lastMessageTime >= maxInterval) {
-      lastMessageTime = now;
-
-      const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
-      const telegramChatId = process.env.TELEGRAM_CHAT_ID;
-
-      try {
-        await axios.post(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
-          chat_id: telegramChatId,
-          text: message,
-        });
-        console.log('Mensagem enviada para o grupo do Telegram com sucesso.');
-      } catch (error) {
-        console.error('Erro ao enviar mensagem para o grupo do Telegram:', error.message);
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${telegramBotToken}/sendMessage`,
+      {
+        chat_id: telegramChatId,
+        text: message,
       }
+    );
+    if (response.data.ok) {
+      console.log("Mensagem enviada para o grupo do Telegram com sucesso.");
     } else {
-      console.log('Aguarde o intervalo mínimo para enviar uma nova mensagem.');
+      console.error("Erro ao enviar mensagem para o grupo do Telegram:", response.data.description);
     }
+  } catch (error) {
+    console.error("Erro ao enviar mensagem para o grupo do Telegram:", error.message);
   }
 }
 
 async function checkSiteStatus(siteUrl) {
-  try {
-    const response = await axios.get(`https://updown.io/api/checks?api-key=${updownApiKey}`);
-    const siteCheck = response.data.find(check => check.url === siteUrl);
-
-    if (siteCheck) {
-      const status = siteCheck.down ? "Offline" : "Online";
-      return status;
-    } else {
-      console.error("O site não foi encontrado no Updown.io");
-      return "Unknown";
+  let attempt = 0;
+  while (attempt < retryAttempts) {
+    try {
+      await axios.get(siteUrl, { httpsAgent: agent });
+      return "Online";
+    } catch (error) {
+      attempt++;
+      console.error(`[${new Date().toLocaleString()}] Tentativa ${attempt} para ${siteUrl} falhou. Erro: ${error.message}`);
+      if (attempt >= retryAttempts) {
+        return "Offline";
+      }
+      await new Promise(resolve => setTimeout(resolve, retryDelay));
     }
-  } catch (error) {
-    console.error("Erro ao verificar status do site através do Updown.io:", error.message);
-    return "Error";
   }
 }
 
@@ -86,14 +76,12 @@ async function checkSitesAndSendAlert() {
     for (const link of links) {
       const siteUrl = link.link;
       const newState = await checkSiteStatus(siteUrl);
-
+      
       if (siteStates[siteUrl] !== newState) {
         if (newState === "Online") {
           await sendTelegramMessage(`O site ${siteUrl} está de volta online.`);
-        } else if (newState === "Offline") {
-          await sendTelegramMessage(`Um ou mais sites estão fora do ar!\n\nO site ${siteUrl} está inacessível.`);
         } else {
-          await sendTelegramMessage(`O site ${siteUrl} tem um status desconhecido. Verifique manualmente.`);
+          await sendTelegramMessage(`Um ou mais sites estão fora do ar!\n\nO site ${siteUrl} está inacessível.`);
         }
         siteStates[siteUrl] = newState;
       }
@@ -135,15 +123,15 @@ app.post("/addLink", async (req, res) => {
     const newLink = new Links({ link });
     await newLink.save();
 
+    // Verificar o estado do site e enviar notificação imediatamente
     const initialState = await checkSiteStatus(link);
     siteStates[link] = initialState;
 
+    // Enviar mensagem ao Telegram informando o estado do site
     if (initialState === "Online") {
       await sendTelegramMessage(`Novo site adicionado e está online: ${link}`);
-    } else if (initialState === "Offline") {
-      await sendTelegramMessage(`Novo site adicionado, mas está inacessível: ${link}`);
     } else {
-      await sendTelegramMessage(`O site ${link} foi adicionado, mas seu status é desconhecido.`);
+      await sendTelegramMessage(`Novo site adicionado, mas está inacessível: ${link}`);
     }
 
     res.status(201).json({ message: "Link adicionado com sucesso" });
@@ -153,8 +141,10 @@ app.post("/addLink", async (req, res) => {
   }
 });
 
+// Chamada inicial para verificar os sites
 checkSitesAndSendAlert();
 
+// Intervalo para verificar os sites a cada 5 minutos
 setInterval(checkSitesAndSendAlert, 5 * 60 * 1000);
 
 connectDataBase()
